4404740dbc508523fa1621c7644adc00955cd7fe
Rana <aulianarana@gmail.com>
2022-12-11 13:27:33 +0800

    404 page

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ app.use(
 );
 app.use("/", indexRouter);
 
+// 404 page
+app.get("*", function (req, res) {
+  res.send("oops! page not found!", 404);
+});
+
 // mongodb
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
@@ -46,21 +51,5 @@ mongoose
   .catch((err) => {
     console.log("disconnect", err);
   });
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports = app;
